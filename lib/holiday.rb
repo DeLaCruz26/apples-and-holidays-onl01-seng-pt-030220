99861f00cfbd0d5bb9f9bec25ee61e9096950274
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-15 07:42:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts
+    puts "#{season.to_s.capitalize}:" 
+    holiday.each do |event, supplies|
+      puts "#{event.to_s.capitalize}:"
   end
 end
 
