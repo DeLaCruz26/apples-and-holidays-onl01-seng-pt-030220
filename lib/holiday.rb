7afc4d5b4cdcedd2e3989cb880ca89eab33f5f2b
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-15 08:05:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,5 +69,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash
+  holiday_hash.collect do |season, holidays|
+    holidays.collect do |event, supplies|
+      if supplies.include?("BBQ")
+        event
+      end
+    end
+  end
 end
